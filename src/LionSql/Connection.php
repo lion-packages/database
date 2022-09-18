919da601922aa303b84573566b711438c3095f9c
Sergio León <sergioleon4004@hotmail.com>
2022-09-18 18:35:14 -0500

    Style: comments deleted

--- src/LionSql/Connection.php
+++ src/LionSql/Connection.php
@@ -37,18 +37,13 @@ class Connection {
 				$host = "mysql:host=" . $config['host'] . ";dbname=" . $config['db_name'];
 			}
 
-			// $host = $host . (isset($config['charset']) ? ";charset={$config['charset']}" : '');
-
 			self::$conn = new PDO(
 				$host,
 				$config['user'],
 				$config['password'],
 				isset($config['options']) ? $config['options'] : [
-					PDO::ATTR_EMULATE_PREPARES => false,
-					PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
 					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-					PDO::ATTR_TIMEOUT => 5
+					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
 				]
 			);
 
