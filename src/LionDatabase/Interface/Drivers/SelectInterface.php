781ac38334d89cc6a16a582f73e37088f4154229
Sleon4 <sergioleon4004.dev@hotmail.com>
2025-10-01 09:35:50 -0500

    feat: added support for multiple queries with the 'SELECT' statement

--- src/LionDatabase/Interface/Drivers/SelectInterface.php
+++ src/LionDatabase/Interface/Drivers/SelectInterface.php
@@ -5,14 +5,12 @@ declare(strict_types=1);
 namespace Lion\Database\Interface\Drivers;
 
 /**
- * Implements a method to select data in SQL queries
- *
- * @package Lion\Database\Interface\Drivers
+ * Implements a method to select data in SQL queries.
  */
 interface SelectInterface
 {
     /**
-     * Nests the SELECT statement in the current query
+     * Nests the SELECT statement in the current query.
      *
      * @return self
      */
